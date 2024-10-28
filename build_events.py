332e1b7cd54715a3d919af77463a7f01c5f233f6
Lincoln Cheng <linc.czp@gmail.com>
2024-10-28 13:08:50 +0800

    updated build_events to include only enrollment event, death event (if any), ED but no admission events, ED admission events

--- build_events.py
+++ build_events.py
@@ -3,21 +3,16 @@ import pandas as pd
 from enums import EventType, PatientType, Gender
 from utils import serializeTimestamp
 
-def addToEvents(events, patient, event_type, event_date):
-  # TODO: validate arguments
+def addToEvents(events, patient_id, patient_type, event_type, event_date):
+  events['id'].append(int(patient_id)) # make id int for sortability
 
-  patient_type = patient['patient_type']
-  gender = patient['gender']
-
-  events['id'].append(int(patient['id'])) #make id int for sortability
   events['patient_type'].append(patient_type)
-  events['gender'].append(gender)
-  events['event_type'].append(event_type)
-  events['event_date'].append(event_date)
-
   events['patient_type_description'].append(PatientType(patient_type).name)
-  events['gender_description'].append(Gender(gender).name)
+
+  events['event_type'].append(event_type)
   events['event_type_description'].append(EventType(event_type).name)
+
+  events['event_date'].append(event_date)
 
   return events
 
@@ -29,89 +24,52 @@ with open('results/patients.json', 'r') as f:
 events = {
   'id': [],
   'patient_type': [],
-  'gender': [],
-  'event_type': [],
-  'event_date': [],
-
   'patient_type_description': [],
-  'gender_description': [],
-  'event_type_description': []
+
+  'event_type': [],
+  'event_type_description': [],
+
+  'event_date': []
 }
 
-ane = pd.read_excel('data/ANE.xlsx')
+ane = pd.read_excel('data/A&E.xlsx')
 for index, row in ane.iterrows():
-  id = str(row['record_id'])
-  patient_with_id = patients[id]
-  patient_with_id['id'] = id
+  patient_id = str(row['record_id'])
+  patient_info = patients[patient_id]
 
   addToEvents(
     events,
-    patient_with_id,
-    EventType.ED_ADMIT if row['Discharge_Type_Description'] == 'I/P Admission' else EventType.ED_NOADMIT,
-    serializeTimestamp(row['Admit_Visit_Date'])
+    patient_id,
+    patient_info['patient_type'],
+    EventType.ED_ADMIT if row['Discharge Type Description'] == 'I/P Admission' else EventType.ED_NOADMIT,
+    serializeTimestamp(row['Admit/Visit Date'])
   )
 
-hdu = pd.read_excel('data/HD.xlsx')
-# TODO: clean up "duplicate" HD events
-for index, row in hdu.iterrows():
-  id = str(row['record_id'])
-  patient_with_id = patients[id]
-  patient_with_id['id'] = id
-
-  if row['Movement_Category_Description'] != 'Discharge':
-    addToEvents(
-      events,
-      patient_with_id,
-      EventType.HDU,
-      serializeTimestamp(row['Admit_Date'])
-    )
-
-icu = pd.read_excel('data/ICU.xlsx')
-for index, row in icu.iterrows():
-  id = str(row['record_id'])
-  patient_with_id = patients[id]
-  patient_with_id['id'] = id
-
-  if row['Movement_Category_Description'] != 'Discharge':
-    addToEvents(
-      events,
-      patient_with_id,
-      EventType.ICU,
-      serializeTimestamp(row['Admit_Date'])
-    )
-
-inpatient = pd.read_excel('data/Inpatient.xlsx')
-for index, row in inpatient.iterrows():
-  id = str(row['record_id'])
-  patient_with_id = patients[id]
-  patient_with_id['id'] = id
+enrollment = pd.read_excel('data/SPARKLE_enrollment.xlsx')
+for index, row in enrollment.iterrows():
+  patient_id = str(row['record_id'])
+  patient_info = patients[patient_id]
 
   addToEvents(
     events,
-    patient_with_id,
-    EventType.INPATIENT,
-    serializeTimestamp(row['Admit_Visit_Date'])
-  )
-
-death = pd.read_excel('data/Death_Details.xlsx')
-for index, row in death.iterrows():
-  id = str(row['record_id'])
-  patient_with_id = patients[id]
-  patient_with_id['id'] = id
-
-  addToEvents(
-    events,
-    patient_with_id,
+    patient_id,
+    patient_info['patient_type'],
     EventType.ENROLLMENT,
     serializeTimestamp(row['Appt_Date'])
   )
 
-  if not pd.isnull(row['Date_of_Death']):
+death = pd.read_excel('data/Death Data.xlsx')
+for index, row in death.iterrows():
+  patient_id = str(row['Record_id'])
+  patient_info = patients[patient_id]
+
+  if not pd.isnull(row['Deathdate']):
     addToEvents(
       events,
-      patient_with_id,
+      patient_id,
+      patient_info['patient_type'],
       EventType.DEATH,
-      serializeTimestamp(row['Date_of_Death'])
+      serializeTimestamp(row['Deathdate'])
     )
 
 events_df = pd.DataFrame(data=events)
