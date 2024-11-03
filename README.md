7b52182fc8f877cef611daf8c6ae3bfaeb4346df
Lincoln Cheng <linc.czp@gmail.com>
2024-11-03 14:47:57 +0800

    refactored build_patients.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 censor date = 30 Apr 2024
 
+patient 109 is excluded.
--- build_patients.py
+++ build_patients.py
@@ -2,30 +2,57 @@ import pandas as pd
 import json
 from enums import PatientType
 
-def upsertPatient(patients, id, patient_type):
-  #TODO: validate arguments
+class PatientInfo:
+  """
+  This class patient information.
 
-  #Create Patient if patient does not exist
-  if not id in patients:
-    patients[id] = {
-      'patient_type': None
-    }
+  Attributes:
+    id (int): The ID of the patient
+    patient_type (PatientType): Whether the patient is randomized to SPARKLE or Usual intervention
+  """
 
-  if patient_type is not None:
-    patients[id]['patient_type'] = patient_type
+  def __init__(self, id, patient_type):
+    """
+    Parameters:
+      id (int): The ID of the patient
+      patient_type (PatientType): Whether the patient is randomized to SPARKLE or Usual intervention
+    """
+    self.id = id
+    self.patient_type = patient_type
 
-  return
+def extractPatientInfo(row):
+  """
+  Extracts patient information from a row in patient_information excel sheet
 
+  Args:
+    row (Series): a row from the excel sheet, represented as a Pandas DataFrame's Series
+
+  Returns:
+    PatientInfo: A PatientInfo object
+  """
+  id = row['REDCap_No']
+  if not type(id) is int:
+    raise ValueError('The id of a patient extracted from the patient_information excel sheet is not an integer type')
+
+  patient_type = row['Combined_data_allocation']
+  if (not patient_type == 'SPARKLE') and (not patient_type == 'Usual'):
+    raise ValueError('The patient type of a patient extracted from the patient_information excel sheet is neither SPARKLE or Usual')
+
+  return PatientInfo(
+    id,
+    PatientType.SPARKLE if row['Combined_data_allocation'] == 'SPARKLE' else PatientType.USUAL
+  )
+
+# -------
 patients = {}
 
-# Is a patient SPARKLE or normal?
-patient_info = pd.read_excel('data/Allocated groups of patients.xlsx')
-for index, row in patient_info.iterrows():
-  upsertPatient(
-    patients,
-    id=row['REDCap_No'],
-    patient_type=PatientType.SPARKLE if row['Combined_data_allocation'] == 'SPARKLE' else PatientType.USUAL
-  )
+patients_info = pd.read_excel('data/patient_information.xlsx')
+for index, row in patients_info.iterrows():
+  patient_info = extractPatientInfo(row)
+
+  patients[str(patient_info.id)] = {
+    'patient_type': patient_info.patient_type
+  }
 
 with open('processed_data/patients.json', 'w') as f:
     json.dump(patients, f, sort_keys=True, indent=2)
